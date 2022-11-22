bdad93bdd35999e92f3daaee36423f671a7ccfe3
mahynotch <minecraftmahynotch@163.com>
2022-11-22 19:52:40 +0800

    Set classes

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,8 +1,2 @@
 public class Coordinate {
-    private int value;
-    Coordinate coordinate;
-
-    public Coordinate(int value, int x, int y){
-
-    }
 }
---
+++ src/Piece.java
@@ -0,0 +1,8 @@
+public class Piece {
+    private int value;
+    Coordinate coordinate;
+
+    public Piece(int value, int x, int y){
+
+    }
+}
