df21342cc8afa81ec6ae8947ca5ec203ce279212
mahynotch <minecraftmahynotch@163.com>
2022-11-24 19:44:00 +0800

    Minor Changes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.Scanner;
-
+/*
+可通过运行文件夹下的test.bat测试输入
+ */
 
 public class Main {
     public static void main(String[] args) {
@@ -15,13 +17,15 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int row = scanner.nextInt();
         int column = scanner.nextInt();
+        //这个board用以储存所有的块
         int[][] board = new int[row][column];
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < column; j++) {
                 board[i][j] = scanner.nextInt();
             }
         }
-        int morbidLength = scanner.nextInt();
+        int morbidLength = scanner.nextInt();//这个变量是特殊块数量
+        //这个hashmap用以储存特殊块，key为块名，value为类型
         HashMap<Integer, String> hashMap = new HashMap<>();
         for (int i = 0; i < morbidLength; i++) {
             hashMap.put(scanner.nextInt(), scanner.next());
