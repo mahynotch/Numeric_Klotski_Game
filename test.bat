f48941e1d9c33ad1617493ef38af095889c8250a
mahynotch <minecraftmahynotch@163.com>
2022-11-26 19:54:37 +0800

    Display the board

--- test.bat
+++ test.bat
@@ -1,13 +1,6 @@
 @echo off
-if "%OS%"=="Windows_NT" setlocal enabledelayedexpansion
-set "bastPath=%~dp0"
-set allFile=
-for /r %%i in (./src/*.java) do (
-    set "var=%%i"
-    set "allFile=!allFile! ./src/!var:%bastPath%=!"
-)
-echo %allFile%
-javac -encoding utf-8  --source-path %allFile% -d ./
+
+javac -encoding utf-8 -d ./ ./src/*.java
 java Main < ./test/1.txt
 for %%i in (.\*.class) do (del %%i)
 pause
