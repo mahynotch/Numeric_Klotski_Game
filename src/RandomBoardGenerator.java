a0a2b583a314eeccd4ae6636681514e0d879992b
mahynotch <minecraftmahynotch@163.com>
2022-11-30 10:20:53 +0800

    Minor modify, todo: complete RandomBoardGenerator

--- src/RandomBoardGenerator.java
+++ src/RandomBoardGenerator.java
@@ -7,7 +7,11 @@ public class RandomBoardGenerator {
     public static void main(String[] args) {
         Random random = new Random();
         int[][] pieceArray = arrayGenerate(random.nextInt(5) + 1, random.nextInt(5) + 1);
-
+        int numOfMorbid = random.nextInt(4);
+        int[] morbids = new int[numOfMorbid];
+        for (int i = 0; i < numOfMorbid; i++) {
+            morbids[i] = random.nextInt(pieceArray.length * pieceArray[0].length);
+        }
     }
 
     private static int[][] arrayGenerate(int row, int column) {
@@ -26,5 +30,16 @@ public class RandomBoardGenerator {
             }
         }
         return res;
+    }
+
+    public static int[] findValue(int[][] pieceArray, int value) {
+        for (int i = 0; i < pieceArray.length; i++) {
+            for (int j = 0; j < pieceArray[0].length; j++) {
+                if (pieceArray[i][j] == value) {
+                    return new int[]{j, i};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No such value in array: " + value);
     }
 }
