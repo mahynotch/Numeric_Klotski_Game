5574db118a1050d90b59e64374a3fa7bbd498e53
eonacz <12110847@mail.sustech.edu.cn>
2022-12-06 17:41:08 +0800

    Update Piece.java
    封装修改

--- src/Piece.java
+++ src/Piece.java
@@ -1,13 +1,18 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 //本类为棋子
 public class Piece implements Cloneable {
     private int[] value;
     Coordinate[] coordinate;
     PieceType pieceType;
+    String code;
 
     public Piece(int[] value, PieceType pieceType, Coordinate[] coordinate) {
         this.value = value;
         this.coordinate = coordinate;
         this.pieceType = pieceType;
+        this.code = code;
     }
 
     public void move(Direction direction) {
@@ -40,23 +45,43 @@ public class Piece implements Cloneable {
         return value;
     }
 
-    public Coordinate[] cloneOfCoordinates() {
-        Coordinate[] newCoord = new Coordinate[coordinate.length];
-        for (int i = 0; i < coordinate.length; i++) {
-            try {
-                newCoord[i] = (Coordinate) coordinate[i].clone();
-            } catch (CloneNotSupportedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return newCoord;
+    public Piece() {
     }
 
     @Override
-    protected Object clone() throws CloneNotSupportedException {
-        Piece clone = (Piece) super.clone();
-        clone.value = value.clone();
-        clone.coordinate = cloneOfCoordinates();
+    protected Piece clone() {
+        Piece clone = new Piece();
+        if (value != null) {
+            clone.value = new int[value.length];
+            System.arraycopy(value, 0, clone.value, 0, value.length);
+        }
+        if (coordinate != null) {
+            clone.coordinate = new Coordinate[coordinate.length];
+            for (int i = 0; i < coordinate.length; i++) {
+                clone.coordinate[i] = coordinate[i].clone();
+            }
+        }
+        clone.pieceType = pieceType;
+        clone.code = code;
         return clone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Piece)) return false;
+        Piece piece = (Piece) o;
+        return code.equals(piece.code) &&
+                Arrays.equals(value, piece.value) &&
+                Arrays.equals(coordinate, piece.coordinate) &&
+                pieceType == piece.pieceType;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(pieceType, code);
+        result = 31 * result + Arrays.hashCode(value);
+        result = 31 * result + Arrays.hashCode(coordinate);
+        return result;
     }
 }
